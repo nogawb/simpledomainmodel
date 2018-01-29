5c429dba4ccf4080dc4f93a701aa3634afd6c4ba
Benjamin Nogawa <benjamin.nogawa@gmail.com>
2018-01-29 15:06:53 -0800

    incase it is broken

--- SimpleDomainModel/SimpleDomainModel/main.swift
+++ SimpleDomainModel/SimpleDomainModel/main.swift
@@ -27,13 +27,49 @@ public struct Money {
   public var amount : Int
   public var currency : String
   
-  public func convert(_ to: String) -> Money {
-  }
-  
-  public func add(_ to: Money) -> Money {
-  }
-  public func subtract(_ from: Money) -> Money {
-  }
+    public mutating func convert(_ to: String) -> Money {
+        var ret = Money(amount: amount, currency: currency)
+        if currency == to {
+            return ret
+        } else if currency != "USD"{ //converting all money to USD
+            
+            if to == "GBP" { //pounds
+                ret.amount = ret.amount * 2
+            } else if currency == "EUR" { //euro
+                ret.amount = (ret.amount * 3) / 2
+            } else { //canadian
+                ret.amount = (ret.amount * 5) / 4
+            }
+        }
+        if to == "USD" {
+            return Money(amount: amount, currency: currency)
+        } else if to == "GBP" {
+            ret.amount = ret.amount * 2
+        } else if to == "EUR" {
+            ret.amount = (ret.amount / 2) * 3
+        } else if to == "CAN"{
+            ret.amount = (ret.amount / 4) * 5
+        } else {
+            print("INVALID CURRENCY YOU DUMMY")
+        }
+        return ret
+    }
+    
+    public mutating func add(_ to: Money) -> Money {
+        var ret = Money(amount: self.amount, currency: currency)
+        ret.convert(to.currency)
+        ret.amount = ret.amount + to.amount
+        ret.convert(self.currency)
+        return ret
+    }
+    
+    public func subtract(_ from: Money) -> Money {
+        var ret = Money(amount: self.amount, currency: currency)
+        ret.convert(from.currency)
+        ret.amount = ret.amount - from.amount
+        ret.convert(self.currency)
+        return ret
+    }
 }
 
 ////////////////////////////////////
